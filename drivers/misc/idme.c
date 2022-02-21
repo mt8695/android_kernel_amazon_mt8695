62f3934ce80537b81fe2edb89a1fa5f14582468e
R0rt1z2 <rogerortizleal@gmail.com>
2022-02-21 14:46:41 +0100

    mt8695: idme: fix return type errors
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- drivers/misc/idme.c
+++ drivers/misc/idme.c
@@ -138,7 +138,7 @@ static s8 castChar(s8 c)
     if(c >= '0' && c <= '9') return c - '0';
     if(c >= 'a' && c <= 'f') return c - 'a' + 10;
     if(c >= 'A' && c <= 'F') return c - 'A' + 10;
-    return 255;
+    return (signed char)255;
 }
 s8 *hexToBytes(s8 *inhex)
 {
